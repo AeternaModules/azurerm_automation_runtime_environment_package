bf9e171cacedbb71f5ba7a25fe599c48f6b4f665
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:46:33 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,41 +1,41 @@
 output "automation_runtime_environment_packages_id" {
   description = "Map of id values across all automation_runtime_environment_packages, keyed the same as var.automation_runtime_environment_packages"
-  value       = { for k, v in azurerm_automation_runtime_environment_package.automation_runtime_environment_packages : k => v.id }
+  value       = { for k, v in azurerm_automation_runtime_environment_package.automation_runtime_environment_packages : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "automation_runtime_environment_packages_automation_runtime_environment_id" {
   description = "Map of automation_runtime_environment_id values across all automation_runtime_environment_packages, keyed the same as var.automation_runtime_environment_packages"
-  value       = { for k, v in azurerm_automation_runtime_environment_package.automation_runtime_environment_packages : k => v.automation_runtime_environment_id }
+  value       = { for k, v in azurerm_automation_runtime_environment_package.automation_runtime_environment_packages : k => v.automation_runtime_environment_id if v.automation_runtime_environment_id != null && length(v.automation_runtime_environment_id) > 0 }
 }
 output "automation_runtime_environment_packages_content_uri" {
   description = "Map of content_uri values across all automation_runtime_environment_packages, keyed the same as var.automation_runtime_environment_packages"
-  value       = { for k, v in azurerm_automation_runtime_environment_package.automation_runtime_environment_packages : k => v.content_uri }
+  value       = { for k, v in azurerm_automation_runtime_environment_package.automation_runtime_environment_packages : k => v.content_uri if v.content_uri != null && length(v.content_uri) > 0 }
 }
 output "automation_runtime_environment_packages_content_version" {
   description = "Map of content_version values across all automation_runtime_environment_packages, keyed the same as var.automation_runtime_environment_packages"
-  value       = { for k, v in azurerm_automation_runtime_environment_package.automation_runtime_environment_packages : k => v.content_version }
+  value       = { for k, v in azurerm_automation_runtime_environment_package.automation_runtime_environment_packages : k => v.content_version if v.content_version != null && length(v.content_version) > 0 }
 }
 output "automation_runtime_environment_packages_default" {
   description = "Map of default values across all automation_runtime_environment_packages, keyed the same as var.automation_runtime_environment_packages"
-  value       = { for k, v in azurerm_automation_runtime_environment_package.automation_runtime_environment_packages : k => v.default }
+  value       = { for k, v in azurerm_automation_runtime_environment_package.automation_runtime_environment_packages : k => v.default if v.default != null }
 }
 output "automation_runtime_environment_packages_hash_algorithm" {
   description = "Map of hash_algorithm values across all automation_runtime_environment_packages, keyed the same as var.automation_runtime_environment_packages"
-  value       = { for k, v in azurerm_automation_runtime_environment_package.automation_runtime_environment_packages : k => v.hash_algorithm }
+  value       = { for k, v in azurerm_automation_runtime_environment_package.automation_runtime_environment_packages : k => v.hash_algorithm if v.hash_algorithm != null && length(v.hash_algorithm) > 0 }
 }
 output "automation_runtime_environment_packages_hash_value" {
   description = "Map of hash_value values across all automation_runtime_environment_packages, keyed the same as var.automation_runtime_environment_packages"
-  value       = { for k, v in azurerm_automation_runtime_environment_package.automation_runtime_environment_packages : k => v.hash_value }
+  value       = { for k, v in azurerm_automation_runtime_environment_package.automation_runtime_environment_packages : k => v.hash_value if v.hash_value != null && length(v.hash_value) > 0 }
 }
 output "automation_runtime_environment_packages_name" {
   description = "Map of name values across all automation_runtime_environment_packages, keyed the same as var.automation_runtime_environment_packages"
-  value       = { for k, v in azurerm_automation_runtime_environment_package.automation_runtime_environment_packages : k => v.name }
+  value       = { for k, v in azurerm_automation_runtime_environment_package.automation_runtime_environment_packages : k => v.name if v.name != null && length(v.name) > 0 }
 }
 output "automation_runtime_environment_packages_size_in_bytes" {
   description = "Map of size_in_bytes values across all automation_runtime_environment_packages, keyed the same as var.automation_runtime_environment_packages"
-  value       = { for k, v in azurerm_automation_runtime_environment_package.automation_runtime_environment_packages : k => v.size_in_bytes }
+  value       = { for k, v in azurerm_automation_runtime_environment_package.automation_runtime_environment_packages : k => v.size_in_bytes if v.size_in_bytes != null }
 }
 output "automation_runtime_environment_packages_version" {
   description = "Map of version values across all automation_runtime_environment_packages, keyed the same as var.automation_runtime_environment_packages"
-  value       = { for k, v in azurerm_automation_runtime_environment_package.automation_runtime_environment_packages : k => v.version }
+  value       = { for k, v in azurerm_automation_runtime_environment_package.automation_runtime_environment_packages : k => v.version if v.version != null && length(v.version) > 0 }
 }
 
